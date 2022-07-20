9aca1c4e278da4075dba1478a95f3858451fa0c5
PauliCrespi <4377@holberton.com>
2022-07-20 11:59:46 -0700

    task 102

---
+++ 102-binary_tree_is_complete.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ *
+ *
+ */
+int binary_tree_is_complete(const binary_tree_t *tree)
+{
+	if (tree == NULL) 
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left != NULL && tree->right == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right != NULL)
+		return (0);
+	return(binary_tree_is_complete(tree->left) && binary_tree_is_complete(tree->right));
+}
